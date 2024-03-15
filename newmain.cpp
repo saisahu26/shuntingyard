8ad1b0f9ca9a7fc701e33144cfd49ad5890c5582
saisahu26 <144160907+saisahu26@users.noreply.github.com>
2024-03-15 09:39:43 -0700

    Update newmain.cpp

--- newmain.cpp
+++ newmain.cpp
@@ -1,10 +1,16 @@
+//Saikapish Sahu
+//3/15/24
+//Shunting Yard
+//Function names and var names/modularity derived from Jazveer K at github.com/JazveerK
+//Print and Queing found from Arjun V and Nikaansh S
+
 #include <iostream>
 #include <cstring>
 #include "node.H"
 
 using namespace std;
 
-// Functions
+// list of functions
 void pushNode(Node* &topNode, Node* newNode);
 Node* popNode(Node* &topNode);
 Node* peekNode(Node* topNode);
@@ -18,58 +24,7 @@ void displayInfix(Node* root);
 void displayPrefix(Node* root);
 void displayPostfix(Node* root);
 
-int main() {
-    char input[100];
-    Node* root = nullptr;
-    Node* stackTopNode = nullptr;
-    Node* queueFrontNode = nullptr;
-    Node* queueRearNode = nullptr;
-    bool running = true;
-    char command[10];
 
-    cout << endl << "Welcome to Expression Evaluation" << endl;
-    
-    while (running) {
-        // Asking user to run program
-        cout << endl << "Enter a command: " << endl;
-        cout << "Calculate" << endl;
-        cout << "Quit" << endl << endl;
-        
-        
-        cin.get(command, 10);
-        cin.clear();
-        cin.ignore(100, '\n');
-        
-        if (strcmp(command, "Quit") == 0) {
-            running = false;
-        }
-        
-        else if (strcmp(command, "Calculate") == 0) {
-            
-            // Input
-            cout << "Enter equation in infix notation: ";
-            cin.getline(input, 100);
-            
-            char postfixExpression[100];
-            convertToPostfix(input, postfixExpression);
-
-            root = constructExpressionTree(postfixExpression);
-            
-            // Output
-            cout << "Postfix Expression: " << postfixExpression << endl;
-            cout << "Infix Notation: ";
-            displayInfix(root);
-            cout << endl;
-            cout << "Prefix Notation: ";
-            displayPrefix(root);
-            cout << endl;
-            cout << "Postfix Notation: ";
-            displayPostfix(root);
-            cout << endl;
-        }
-    }
-    return 0;
-}
 
 // Stack functions
 void pushNode(Node* &topNode, Node* newNode) {
@@ -95,37 +50,45 @@ Node* peekNode(Node* topNode) {
     return topNode;
 }
 
-// Queue
-void enqueueNode(Node* &frontNode, Node* &rearNode, Node* newNode) {
-    if (frontNode == nullptr) {
-        frontNode = rearNode = newNode;
-    } else {
-        rearNode->setNext(newNode);
-        rearNode = newNode;
+// display function being called from node
+void displayInfix(Node* root) {//infix display
+    if (root) {
+        if (isOperator(root->getData()) && root->getData() != '(')
+            cout << "(";
+        displayInfix(root->getLeft());
+        cout << root->getData();
+        displayInfix(root->getRight());
+        if (isOperator(root->getData()) && root->getData() != ')')
+            cout << ")";
     }
 }
 
-Node* dequeueNode(Node* &frontNode) {
-    if (frontNode == nullptr) {
-        cerr << "Queue is empty\n";
-        return nullptr;
+void displayPrefix(Node* root) {//prefix display
+    if (root) {
+        cout << root->getData();
+        displayPrefix(root->getLeft());
+        displayPrefix(root->getRight());
     }
-    Node* data = frontNode;
-    frontNode = frontNode->getNext();
-    return data;
 }
 
-bool isOperator(char c) {
+void displayPostfix(Node* root) { //post fix display
+    if (root) {
+        displayPostfix(root->getLeft());
+        displayPostfix(root->getRight());
+        cout << root->getData();
+    }
+}
+
+bool isOperator(char c) { //set of operaters that will be read
     return (c == '+' || c == '-' || c == '*' || c == '/' || c == '^');
 }
 
-// Precedence with carrots and parenthesis now
-int precedence(char c) {
+
+int precedence(char c) { //order and precedence of what operators will be taken
     if (c == '+' || c == '-')
         return 1;
     else if (c == '*' || c == '/')
         return 2;
-    // ADDED CARROTS AND PARENTHESIS
     else if (c == '^')
         return 3;
     else if (c == '(' || c == ')')
@@ -133,10 +96,10 @@ int precedence(char c) {
     return 0;
 }
 
-// The Actual Shunting Yard Algorithm
-void convertToPostfix(const char* input, char* postfixExpression) {
+
+void convertToPostfix(const char* input, char* postfixExpression) {//main yard algorithm 
     Node* operatorStack = nullptr;
-    Node* outputQueueFrontNode = nullptr;
+    Node* outputQueueFrontNode = nullptr; //setting queues to null
     Node* outputQueueRearNode = nullptr;
     int i = 0;
 
@@ -174,6 +137,7 @@ void convertToPostfix(const char* input, char* postfixExpression) {
     postfixExpression[j] = '\0';
 }
 
+//actual construction of the expression binary tree
 Node* constructExpressionTree(const char* postfixExpression) {
     Node* nodeStack = nullptr;
     int i = 0;
@@ -198,30 +162,77 @@ Node* constructExpressionTree(const char* postfixExpression) {
     return popNode(nodeStack);
 }
 
-void displayInfix(Node* root) {
-    if (root) {
-        if (isOperator(root->getData()) && root->getData() != '(')
-            cout << "(";
-        displayInfix(root->getLeft());
-        cout << root->getData();
-        displayInfix(root->getRight());
-        if (isOperator(root->getData()) && root->getData() != ')')
-            cout << ")";
+
+
+
+//queueing and dequeueing the nodes
+void enqueueNode(Node* &frontNode, Node* &rearNode, Node* newNode) {
+    if (frontNode == nullptr) {
+        frontNode = rearNode = newNode;
+    } else {
+        rearNode->setNext(newNode);
+        rearNode = newNode;
     }
 }
 
-void displayPrefix(Node* root) {
-    if (root) {
-        cout << root->getData();
-        displayPrefix(root->getLeft());
-        displayPrefix(root->getRight());
+Node* dequeueNode(Node* &frontNode) {
+    if (frontNode == nullptr) {
+        cerr << "Queue is empty\n";
+        return nullptr;
     }
+    Node* data = frontNode;
+    frontNode = frontNode->getNext();
+    return data;
 }
 
-void displayPostfix(Node* root) {
-    if (root) {
-        displayPostfix(root->getLeft());
-        displayPostfix(root->getRight());
-        cout << root->getData();
+//main function that cals all the functions and prompts the user
+int main() {
+    char input[100];
+    Node* root = nullptr;
+    Node* stackTopNode = nullptr;
+    Node* queueFrontNode = nullptr;
+    Node* queueRearNode = nullptr;
+    bool running = true;
+    char command[10];
+    
+    while (running) {
+        // user prompt
+        cout << endl << "input an expression: " << endl;
+        cout << "Calculate" << endl; //names from Jazveer's 'main.cpp' from github.com/JazveerK
+        cout << "Quit" << endl << endl;
+        
+        
+        cin.get(command, 10);
+        cin.clear();
+        cin.ignore(100, '\n');
+        
+        if (strcmp(command, "Quit") == 0) {
+            running = false;
+        }
+        
+        else if (strcmp(command, "Calculate") == 0) {
+            
+            // Input
+            cout << "Enter equation in infix notation: ";
+            cin.getline(input, 100);
+            
+            char postfixExpression[100];
+            convertToPostfix(input, postfixExpression);
+
+            root = constructExpressionTree(postfixExpression);
+            
+            // printing all of the fixes based on the input
+            cout << "Postfix Expression: " << postfixExpression << endl;
+            cout << "Infix Notation: ";
+            displayInfix(root);
+            cout << endl;
+            cout << "Prefix Notation: ";
+            displayPrefix(root);
+            cout << endl;
+            cout << "Postfix Notation: ";
+            displayPostfix(root);
+            cout << endl;
+        }
     }
+    return 0;
 }
