9ec4d1396174f40dcc76a87753d5a3e340c96f46
saisahu26 <144160907+saisahu26@users.noreply.github.com>
2024-03-15 00:21:15 -0700

    Add files via upload

---
+++ newnode.cpp
@@ -0,0 +1,59 @@
+//sai sahu
+//modulairty from Jazveer K.
+//var names from Arjun V and Nikaansh S
+
+#include <iostream>
+#include <cstring>
+#include "node.h"
+
+using namespace std;
+
+
+
+// classifier and implimenting
+Node::Node() {
+    data = '\0';
+    //creating pointers for all directions (name from Jazveer K)
+    next = nullptr;
+    left = nullptr;
+    right = nullptr;
+}
+
+Node::Node(char newData) {
+    data = newData;
+    next = nullptr;
+    left = nullptr;
+    right = nullptr;
+}
+//creating getters and setters for all sides and the data values
+char Node::getData() {
+    return data;
+}
+
+Node* Node::getNext() {
+    return next;
+}
+
+Node* Node::getLeft() {
+    return left;
+}
+
+Node* Node::getRight() {
+    return right;
+}
+
+void Node::setData(char newData) {
+    data = newData;
+}
+
+void Node::setNext(Node* newNext) {
+    next = newNext;
+}
+
+void Node::setLeft(Node* newLeft) {
+    left = newLeft;
+}
+
+void Node::setRight(Node* newRight) {
+    right = newRight;
+}
